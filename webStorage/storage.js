c515d7c73fa50187d0374989e134a98daee7bdf9
ychao <290388252@qq.com>
2017-08-11 16:51:41 +0800

    in company change

---
+++ webStorage/storage.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Created by Administrator on 2017/8/11.
+ */
+function saveStorage(id) {
+    var target = document.getElementById(id);
+    var str = target.value;
+    sessionStorage.setItem("mess",str);
+}
+function loadStorage(id) {
+    var target = document.getElementById(id);
+    var msg = sessionStorage.getItem("mess");
+    target.innerHTML = msg;
+
+}
+function saveStorage2(id) {
+    var target = document.getElementById(id);
+    var str = target.value;
+    localStorage.setItem("mess",str);
+}
+function loadStorage2(id) {
+    var target = document.getElementById(id);
+    var msg = localStorage.getItem("mess");
+    target.innerHTML = msg;
+
+}
