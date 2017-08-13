6cab4fd2ae09b39af0ec7939665f7fe3260e4bc8
yanchao <290388252@qq.com>
2017-08-13 23:19:37 +0800

    0813commit

--- textarea/textarea.js
+++ textarea/textarea.js
@@ -1,30 +1,32 @@
 /**
+ *
  * Created by Administrator on 2017/8/11.
  */
 function saveStorage(id) {
-    var target = document.getElementById(id).value;
+    var date = document.getElementById(id).value;
     var time = new Date().getTime();
 
-    localStorage().setItem(time,target);
+    localStorage.setItem(time,date);
     alert("数据已经被保存");
     loadStorage('msg');
 }
 function loadStorage(id) {
     var reselt = '<table border="1">';
     for (var i = 0; i<localStorage.length;i++){
-        var key = localStorage.key(1);
+        var key = localStorage.key(i);
         var value = localStorage.getItem(key);
         var date = new Date();
         date.setTime(key);
         var datestr = date.toGMTString();
-        reselt +='<tr><td>'+'这是第'+i+'条数据</td><td>'+value+'</td><td>'+datestr+'</td></tr>'
-
+        reselt +='<tr><td>'+'这是第'+i+'条数据</td><td>'+value+'</td><td>'+datestr+'</td></tr>';
     }
     reselt += '</table>';
     var target = document.getElementById(id);
     target.innerHTML = reselt;
 }
+
 function cleanStorage(id) {
     localStorage.clear();
     alert("数据已经被清空");
+    loadStorage('msg')
 }
