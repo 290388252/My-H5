68f0a21a6b07dc7bb0b717fe3ce07a6733099b45
ychao <290388252@qq.com>
2017-09-13 17:29:39 +0800

    0912

--- javascript/pubuliu/app.js
+++ javascript/pubuliu/app.js
@@ -10,8 +10,28 @@ function imgLoacation(parent,content) {
     var ccontent = getChildElement(cparent,content);
     var imgWidth = ccontent[0].offsetWidth;
     var cols = document.documentElement.clientWidth/imgWidth;//屏幕宽度/图片宽度 = 屏幕所能盛放图片的个数
-    var colsM = Math.floor(cols);//转化整数
-    cparent.style.cssText = 'width:' + imgWidth*cols + 'px;margin：0 auto;'
+    var num = Math.floor(cols);//转化整数
+    cparent.style.cssText = 'width:' + imgWidth*num + 'px;margin：0 auto;'
+    var boxHeightArr = [];
+    for(var i =0;i<ccontent.length;i++){
+        if (i<num) {
+            boxHeightArr[i] = ccontent[i].offsetHeight;
+        }else {
+            var minHeight = Math.min.apply(null,boxHeightArr);
+            var index = getMinHeightLocation(boxHeightArr,minHeight);
+            ccontent[i].style.position = 'absolute';
+            ccontent[i].style.top = minHeight + 'px;'
+            ccontent[i].style.left = ccontent[index].offsetLeft
+        }
+    }
+}
+
+function getMinHeightLocation(boxHeightArr,minHeight) {
+    for(var i in boxHeightArr){
+        if (boxHeightArr[i] == minHeight){
+            return i
+        }
+    }
 }
 
 function getChildElement(parent,content) {
